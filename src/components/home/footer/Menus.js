0488c1e54b570795e09dba8dda34c9f619fbe276
unknown <genxhatr@hotmail.com>
2023-03-29 15:37:42 -0300

    footer

---
+++ src/components/home/footer/Menus.js
@@ -0,0 +1,32 @@
+export const PRODUCTS = [
+    { name: "Producto 1", link: "#" },
+    { name: "Producto 2", link: "#" },
+    { name: "Producto 3", link: "#" },
+  ];
+  export const RESOURCES = [
+    { name: "Informacion", link: "#" },
+    { name: "Motivo", link: "#" },
+    { name: "Blog", link: "#" },
+    { name: "Eventos", link: "#" },
+  ];
+  export const COMPANY = [
+    { name: "Compania 1", link: "#" },
+    { name: "Compania 2", link: "#" },
+    { name: "Compania 3", link: "#" },
+  ];
+  export const SUPPORT = [
+    { name: "Documentacion", link: "#" },
+    { name: "Tutoriales y guías", link: "#" },
+    { name: "Seminarios web", link: "#" },
+    { name: "Fuente abierta", link: "#" },
+  ];
+  
+  export const Icons = [
+    { name: "logo-facebook", link: "#" },
+    { name: "logo-twitter", link: "#" },
+    { name: "logo-github", link: "#" },
+    { name: "logo-linkedin", link: "#" },
+    { name: "logo-instagram", link: "#" },
+  ];
+  
+  
--- src/components/home/footer/footer.jsx
+++ src/components/home/footer/footer.jsx
@@ -1,43 +1,44 @@
 import React from "react";
+import ItemsContainer from "./ItemsContainer";
+import SocialIcons from "./SocialIcons";
+import { Icons } from "./Menus";
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <div>
-      <div>
-        <a href="https://www.facebook.com/BastetRescate" target="_blank">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          role="img"
-          width="2em"
-          height="2em"
-          viewBox="0 0 24 24"
+    <footer className="bg-gray-900 text-white">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+        <h1
+          className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
+         md:w-2/5"
         >
-          <path
-            fill="currentColor"
-            d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"
-          ></path>
-        </svg>
-        </a>
-        <a href="https://www.instagram.com/bastetrescate/?hl=es" target="_blank">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          role="img"
-          width="2em"
-          height="2em"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="currentColor"
-            d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"
-          ></path>
-        </svg>
-        </a>
+          <span className="text-teal-400">Bastet </span> tu gatito te espera
+        </h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Correo"
+            className="text-gray-800
+           sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
+          />
+          <button
+            className="bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[Poppins]
+           rounded-md text-white md:w-auto w-full"
+          >
+            Enviar
+          </button>
+        </div>
       </div>
-      <div>
-        <h1>Contacto</h1>
+      <ItemsContainer />
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
+      text-center pt-2 text-gray-400 text-sm pb-8"
+      >
+        <span>© 2023 Bastet . Reservados todos los derechos.</span>
+        <span>Términos · política de privacidad</span>
+        <SocialIcons Icons={Icons} />
       </div>
-    </div>
+    </footer>
   );
-}
+};
+
+export default Footer;
