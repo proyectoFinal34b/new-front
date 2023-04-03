0b4a962746aff0448ad03beec0e17eb780af0686
Bchialva <barbarachialva@gmail.com>
2023-04-03 11:49:04 -0300

    va otro

--- src/components/filtros/filtros.jsx
+++ src/components/filtros/filtros.jsx
@@ -1,46 +1,142 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-const Gatos = [
-  { id: 1, nombre: 'Mittens', status: 'Adoptado', edad: 2, sexo: 'Hembra' },
-  { id: 2, nombre: 'Toby', status: 'En albergue', edad: 4, sexo: 'Macho' },
-  { id: 3, nombre: 'Socks', status: 'Apadrinado', edad: 3, sexo: 'Hembra' },
-  { id: 4, nombre: 'Oliver', status: 'Adoptado', edad: 1, sexo: 'Macho' },
-  { id: 5, nombre: 'Luna', status: 'Apadrinado', edad: 5, sexo: 'Hembra' },
-  { id: 6, nombre: 'Simba', status: 'En albergue', edad: 2, sexo: 'Macho' },
-  { id: 7, nombre: 'Ginger', status: 'En albergue', edad: 4, sexo: 'Hembra' },
-  { id: 8, nombre: 'Whiskers', status: 'Adoptado', edad: 3, sexo: 'Macho' },
-  { id: 9, nombre: 'Milo', status: 'En albergue', edad: 2, sexo: 'Macho' },
-  { id: 10, nombre: 'Misty', status: 'Apadrinado', edad: 1, sexo: 'Hembra' },
-];
+// const Gatos = [
+//   { id: 1, nombre: 'Mittens', status: 'Adoptado', edad: 2, sexo: 'Hembra' },
+//   { id: 2, nombre: 'Toby', status: 'En albergue', edad: 4, sexo: 'Macho' },
+//   { id: 3, nombre: 'Socks', status: 'Apadrinado', edad: 3, sexo: 'Hembra' },
+//   { id: 4, nombre: 'Oliver', status: 'Adoptado', edad: 1, sexo: 'Macho' },
+//   { id: 5, nombre: 'Luna', status: 'Apadrinado', edad: 5, sexo: 'Hembra' },
+//   { id: 6, nombre: 'Simba', status: 'En albergue', edad: 2, sexo: 'Macho' },
+//   { id: 7, nombre: 'Ginger', status: 'En albergue', edad: 4, sexo: 'Hembra' },
+//   { id: 8, nombre: 'Whiskers', status: 'Adoptado', edad: 3, sexo: 'Macho' },
+//   { id: 9, nombre: 'Milo', status: 'En albergue', edad: 2, sexo: 'Macho' },
+//   { id: 10, nombre: 'Misty', status: 'Apadrinado', edad: 1, sexo: 'Hembra' },
+// ];
+
+// export default function GatosFiltrados() {
+//   const [gatos, setGatos] = useState(Gatos);
+//   const [filtroStatus, setFiltroStatus] = useState("");
+//   const [filtroSexo, setFiltroSexo] = useState("");
+//   const [filtroEdad, setFiltroEdad] = useState("");
+
+//   function aplicarFiltros(status, sexo, edad)  {
+//     let tempGatos = Gatos;
+  
+//     if (status) {
+//       tempGatos = tempGatos.filter((gato) => gato.status === filtroStatus);
+//     }
+//     if (sexo) {
+//       tempGatos = tempGatos.filter((gato) => gato.sexo === filtroSexo);
+//     }
+//     if (edad) {
+//       tempGatos = tempGatos.filter((gato) => gato.edad === parseInt(filtroEdad));
+//     }
+//     setGatos(tempGatos);
+//   }
+
+//   useEffect(() => {
+//     aplicarFiltros(filtroStatus, filtroSexo, filtroEdad);
+//   }, [filtroStatus, filtroSexo, filtroEdad]);
+
+//   return (
+//     <div>
+//       <h2>Gatos en adopción</h2>
+//       <div>
+//         <label htmlFor="status">Estado:</label>
+//         <select
+//           id="status"
+//           onChange={(e) => setFiltroStatus(e.target.value)}
+//           value={filtroStatus}
+//         >
+//           <option value="">Todos los gatos</option>
+//           <option value="En albergue">En Albergue</option>
+//           <option value="Adoptado">Adoptado</option>
+//           <option value="Apadrinado">Apadrinado</option>
+//         </select>
+//       </div>
+//       <div>
+//         <label htmlFor="sexo">Sexo:</label>
+//         <select
+//           id="sexo"
+//           onChange={(e) => setFiltroSexo(e.target.value)}
+//           value={filtroSexo}
+//         >
+//           <option value="">Cualquier Sexo</option>
+//           <option value="Hembra">Hembra</option>
+//           <option value="Macho">Macho</option>
+//         </select>
+//       </div>
+//       <div>
+//         <label htmlFor="edad">Edad:</label>
+//         <select
+//           id="edad"
+//           onChange={(e) => setFiltroEdad(e.target.value)}
+//           value={filtroEdad}
+//         >
+//           <option value="">Cualquier edad</option>
+//           <option value="1">1 año</option>
+//           <option value="2">2 años</option>
+//           <option value="3">3 años</option>
+//           <option value="4">4 años</option>
+//           <option value="5">5 años</option>
+//         </select> 
+//       </div>
+//       <ul>
+//         {gatos.map((gato) => (
+//           <li key={gato.id}>
+//             <p>Nombre: {gato.nombre}</p>
+//             <p>Raza: {gato.status}</p>
+//             <p>Sexo: {gato.sexo}</p>
+//             <p>Edad: {gato.edad} años</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { filterCats, getCats } from "../../redux/actions";
 
 export default function GatosFiltrados() {
-  const [gatos, setGatos] = useState(Gatos);
+  const dispatch = useDispatch();
   const [filtroStatus, setFiltroStatus] = useState("");
-  const [filtroSexo, setFiltroSexo] = useState("");
-  const [filtroEdad, setFiltroEdad] = useState("");
+  const [filtroGender, setFiltroGender] = useState("");
+  const [filtroAge, setFiltroAge] = useState("");
 
-  function aplicarFiltros(status, sexo, edad)  {
-    let tempGatos = Gatos;
-  
-    if (status) {
-      tempGatos = tempGatos.filter((gato) => gato.status === filtroStatus);
-    }
-    if (sexo) {
-      tempGatos = tempGatos.filter((gato) => gato.sexo === filtroSexo);
-    }
-    if (edad) {
-      tempGatos = tempGatos.filter((gato) => gato.edad === parseInt(filtroEdad));
-    }
-    setGatos(tempGatos);
-  }
+  const gatos = useSelector((state) => state.cats);
 
   useEffect(() => {
-    aplicarFiltros(filtroStatus, filtroSexo, filtroEdad);
-  }, [filtroStatus, filtroSexo, filtroEdad]);
+    dispatch(getCats());
+  }, [dispatch]);
+
+  function aplicarFiltros(status, gender, age) {
+    let tempGatos = gatos;
+
+    if (status) {
+      tempGatos = tempGatos.filter((gato) => gato.status === status);
+    }
+    if (gender) {
+      tempGatos = tempGatos.filter((gato) => gato.gender === gender);
+    }
+    if (age) {
+      tempGatos = tempGatos.filter((gato) => gato.age === parseInt(age));
+    }
+    
+    return tempGatos;
+  }
+
+  const gatosFiltrados = aplicarFiltros(filtroStatus, filtroGender, filtroAge);
+  
+  
+  useEffect(() => {
+    dispatch(filterCats(gatosFiltrados));
+  }, [filtroStatus, filtroGender, filtroAge]);
 
   return (
     <div>
-      <h2>Gatos en adopción</h2>
+      <h2>Gatos</h2>
       <div>
         <label htmlFor="status">Estado:</label>
         <select
@@ -49,29 +145,29 @@ export default function GatosFiltrados() {
           value={filtroStatus}
         >
           <option value="">Todos los gatos</option>
-          <option value="En albergue">En Albergue</option>
-          <option value="Adoptado">Adoptado</option>
-          <option value="Apadrinado">Apadrinado</option>
+          <option value="albergue">En Albergue</option>
+          <option value="adoptado">Adoptado</option>
+          <option value="apadrinado">Apadrinado</option>
         </select>
       </div>
       <div>
-        <label htmlFor="sexo">Sexo:</label>
+        <label htmlFor="gender">Sexo:</label>
         <select
-          id="sexo"
-          onChange={(e) => setFiltroSexo(e.target.value)}
-          value={filtroSexo}
+          id="gender"
+          onChange={(e) => setFiltroGender(e.target.value)}
+          value={filtroGender}
         >
           <option value="">Cualquier Sexo</option>
-          <option value="Hembra">Hembra</option>
-          <option value="Macho">Macho</option>
+          <option value="hembra">Hembra</option>
+          <option value="macho">Macho</option>
         </select>
       </div>
       <div>
-        <label htmlFor="edad">Edad:</label>
+        <label htmlFor="age">Edad:</label>
         <select
-          id="edad"
-          onChange={(e) => setFiltroEdad(e.target.value)}
-          value={filtroEdad}
+          id="age"
+          onChange={(e) => setFiltroAge(e.target.value)}
+          value={filtroAge}
         >
           <option value="">Cualquier edad</option>
           <option value="1">1 año</option>
@@ -81,16 +177,6 @@ export default function GatosFiltrados() {
           <option value="5">5 años</option>
         </select> 
       </div>
-      <ul>
-        {gatos.map((gato) => (
-          <li key={gato.id}>
-            <p>Nombre: {gato.nombre}</p>
-            <p>Raza: {gato.status}</p>
-            <p>Sexo: {gato.sexo}</p>
-            <p>Edad: {gato.edad} años</p>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,34 +1,75 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-export default function SearchBar(props) {
-  const [searchText, setSearchText] = useState('');
+// export default function SearchBar(props) {
+//   const [searchText, setSearchText] = useState('');
 
-  const handleInputChange = (event) => {
-    setSearchText(event.target.value);
-  };
+//   const handleInputChange = (event) => {
+//     setSearchText(event.target.value);
+//   };
 
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Searching for ${searchText}`);
+//   const handleSearchSubmit = (event) => {
+//     event.preventDefault();
+//     console.log(`Searching for ${searchText}`);
 
-    // Lógica de búsqueda
-    const filteredResults = props.results.filter((result) => {
-      return result.title.toLowerCase().includes(searchText.toLowerCase());
-    });
+//     // Lógica de búsqueda
+//     const filteredResults = props.results.filter((result) => {
+//       return result.title.toLowerCase().includes(searchText.toLowerCase());
+//     });
 
-    props.onSearch(filteredResults);
-  };
+//     props.onSearch(filteredResults);
+//   };
+
+//   return (
+//     <form onSubmit={handleSearchSubmit}>   
+//     <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+//       <div class="relative">
+//         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+//             <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+//         </div>
+//         <input type="search" id="default-search" onChange={handleInputChange} value={searchText} class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required/>
+//         <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+//       </div>
+//   </form>
+// );
+// }
+
+import React from 'react'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { searchCats } from "../../redux/actions";
+
+export default function SearchBar() {
+  const dispatch = useDispatch();
+  const allCats = useSelector(state => state.cats);
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (name) {
+      dispatch(searchCats(name));
+      setName("");
+      if (allCats.length === 0) {
+        alert(`No se encontró ningún gato llamado ${name}`);
+      }
+    } else {
+      alert("Ingrese un gato.");
+    }
+  }
+
+  function handleChange(e) {
+    e.preventDefault();
+    setName(e.target.value);
+  }
 
   return (
-    <form onSubmit={handleSearchSubmit}>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-        <input type="search" id="default-search" onChange={handleInputChange} value={searchText} class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required/>
-        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-      </div>
-    </form>
-  );
+    <div>
+      <input
+        type="text"
+        value={name}
+        placeholder="Gato.."
+        onChange={(e) => handleChange(e)}
+      />
+      <button type="submit" onClick={(e) => handleSubmit(e)}>Buscar</button>
+    </div>
+  );
 }
